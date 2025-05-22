f8f35135c6f789567653c1167729c6f12a439311
Ibrahim <ibrahimsakariyah@gmail.com>
2025-05-22 10:42:27 +0100

    basic arithmetic operations

--- basic_operations.py
+++ basic_operations.py
@@ -1,6 +1,6 @@
 number1 = 10
 number2 = 5
 
-print(f"Addition of {number1} and {number2} is", number1 + number2)
-print(f"Subtraction of {number1} and {number2} is", number1 - number2)
-print(f"Multiplication of {number1} and {number2} is", number1 * number2)
+print("Addition of 10 and 5 is", number1 + number2)
+print("Subtraction of 10 and 5 is", number1 - number2)
+print("Multiplication of 10 and 5 is", number1 * number2)
