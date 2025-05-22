b352864556a677d6d55a567a55c3064ed1728ecf
Ibrahim <ibrahimsakariyah@gmail.com>
2025-05-22 17:50:00 +0100

    file structure

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = {number1}
+number2 = {number2}
+
+print(f"Addition of {number1} and {number2} is", number1 + number2)
+print(f"Subtraction of {number1} and {number2} is", number1 - number2)
+print(f"Multiplication of {number1} and {number2} is", number1 * number2)
